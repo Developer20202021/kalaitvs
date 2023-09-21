622eac27dcbf3915992a59f7d95ce58142f6c11e
developer20202021 <mahadikaushik8888@gmail.com>
2023-09-21 06:03:11 +0600

    create new customer account

---
+++ lib/Screens/AdminScreen/CreateNewCustomer.dart
@@ -0,0 +1,285 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tvs_app/Screens/CommonScreen/LogInScreen.dart';
+
+
+class CreateNewCustomer extends StatefulWidget {
+  const CreateNewCustomer({super.key});
+
+  @override
+  State<CreateNewCustomer> createState() => _CreateNewCustomerState();
+}
+
+class _CreateNewCustomerState extends State<CreateNewCustomer> {
+  TextEditingController myEmailController = TextEditingController();
+  TextEditingController myPassController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    FocusNode myFocusNode = new FocusNode();
+
+
+ 
+
+    return  Scaffold(
+      backgroundColor: Colors.white,
+      
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.purple),
+        leading: IconButton(onPressed: () => Navigator.of(context).pop(), icon: Icon(Icons.chevron_left)),
+        title: const Text("Create Customer Account", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.transparent,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        centerTitle: true,
+        
+      ),
+      body: SingleChildScrollView(
+
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+            
+                    
+                    Center(
+                      child: Lottie.asset(
+                      'lib/images/animation_lk8g4ixk.json',
+                        fit: BoxFit.cover,
+                        width: 300,
+                        height: 200
+                      ),
+                    ),
+            
+            // SizedBox(
+            //           height: 20,
+            //         ),
+            
+            
+            // enter your email 
+            
+                    TextField(
+                      focusNode: myFocusNode,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Customer Name',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Customer Name',
+            
+                          //  enabledBorder: OutlineInputBorder(
+                          //       borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //     ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.purple),
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                              ),
+                          
+                          
+                          ),
+                      controller: myEmailController,
+                    ),
+            
+            
+            
+            
+                    SizedBox(
+                      height: 20,
+                    ),
+            
+            
+            
+            
+            
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Address',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Customer Address',
+                          //  enabledBorder: OutlineInputBorder(
+                          //     borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //   ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 3, color: Colors.purple),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                            ),
+                          
+                          
+                          ),
+                      controller: myPassController,
+                    ),
+            
+                    SizedBox(
+                      height: 10,
+                    ),
+
+
+
+                    SizedBox(
+                      height: 20,
+                    ),
+            
+            
+            
+                    TextField(
+                      focusNode: myFocusNode,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Customer Phone Number',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Customer Phone Number',
+            
+                          //  enabledBorder: OutlineInputBorder(
+                          //       borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //     ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.purple),
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                              ),
+                          
+                          
+                          ),
+                      controller: myEmailController,
+                    ),
+            
+            
+            
+            
+                    SizedBox(
+                      height: 20,
+                    ),
+            
+            
+            
+            
+            
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Customer Email',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Customer Email',
+                          //  enabledBorder: OutlineInputBorder(
+                          //     borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //   ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 3, color: Colors.purple),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                            ),
+                          
+                          
+                          ),
+                      controller: myPassController,
+                    ),
+            
+                    SizedBox(
+                      height: 10,
+                    ),
+
+                   
+
+
+
+
+
+
+
+
+
+
+
+            
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(width: 150, child:TextButton(onPressed: (){
+
+                            Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LogInScreen()),
+                      );
+
+
+
+
+
+                        }, child: Text("Save", style: TextStyle(color: Colors.white),), style: ButtonStyle(
+                         
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.purple),
+              ),),),
+
+
+
+              
+
+
+
+
+
+                      ],
+                    )
+            
+            
+            
+                  ],
+                ),
+              ),
+            ),
+        
+      
+      
+    );
+  }
+}
+
+
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.purple;
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.875,
+        size.width * 0.5, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.9584,
+        size.width * 1.0, size.height * 0.9167);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
---
+++ lib/Screens/CommonScreen/RegistrationScreen.dart
@@ -0,0 +1,314 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tvs_app/Screens/CommonScreen/LogInScreen.dart';
+
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  TextEditingController myEmailController = TextEditingController();
+  TextEditingController myPassController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    FocusNode myFocusNode = new FocusNode();
+
+
+ 
+
+    return  Scaffold(
+      backgroundColor: Colors.white,
+      
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.purple),
+        leading: IconButton(onPressed: () => Navigator.of(context).pop(), icon: Icon(Icons.chevron_left)),
+        title: const Text("Create Your Account", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.transparent,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        centerTitle: true,
+        
+      ),
+      body: SingleChildScrollView(
+
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+            
+                    
+                    Center(
+                      child: Lottie.asset(
+                      'lib/images/animation_lk8g4ixk.json',
+                        fit: BoxFit.cover,
+                        width: 300,
+                        height: 200
+                      ),
+                    ),
+            
+            // SizedBox(
+            //           height: 20,
+            //         ),
+            
+            
+            
+                    TextField(
+                      focusNode: myFocusNode,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Name',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Your Name',
+            
+                          //  enabledBorder: OutlineInputBorder(
+                          //       borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //     ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.purple),
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                              ),
+                          
+                          
+                          ),
+                      controller: myEmailController,
+                    ),
+            
+            
+            
+            
+                    SizedBox(
+                      height: 20,
+                    ),
+            
+            
+            
+            
+            
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Address',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Your Address',
+                          //  enabledBorder: OutlineInputBorder(
+                          //     borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //   ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 3, color: Colors.purple),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                            ),
+                          
+                          
+                          ),
+                      controller: myPassController,
+                    ),
+            
+                    SizedBox(
+                      height: 10,
+                    ),
+
+
+
+                    SizedBox(
+                      height: 20,
+                    ),
+            
+            
+            
+                    TextField(
+                      focusNode: myFocusNode,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Phone Number',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Your Phone Number',
+            
+                          //  enabledBorder: OutlineInputBorder(
+                          //       borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //     ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.purple),
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                              ),
+                          
+                          
+                          ),
+                      controller: myEmailController,
+                    ),
+            
+            
+            
+            
+                    SizedBox(
+                      height: 20,
+                    ),
+            
+            
+            
+            
+            
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Email',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Your Email',
+                          //  enabledBorder: OutlineInputBorder(
+                          //     borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //   ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 3, color: Colors.purple),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                            ),
+                          
+                          
+                          ),
+                      controller: myPassController,
+                    ),
+            
+                    SizedBox(
+                      height: 10,
+                    ),
+
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Password',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Your Password',
+                          //  enabledBorder: OutlineInputBorder(
+                          //     borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //   ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 3, color: Colors.purple),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                            ),
+                          
+                          
+                          ),
+                      controller: myPassController,
+                    ),
+            
+
+            SizedBox(
+                      height: 10,
+                    ),
+
+
+
+
+
+
+
+
+
+
+
+            
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(width: 150, child:TextButton(onPressed: (){
+
+                            Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LogInScreen()),
+                      );
+
+
+
+
+
+                        }, child: Text("Log in", style: TextStyle(color: Colors.white),), style: ButtonStyle(
+                         
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.purple),
+              ),),),
+
+
+
+                    Container(width: 150, child:TextButton(onPressed: (){}, child: Text("Create Account", style: TextStyle(color: Colors.white),), style: ButtonStyle(
+                         
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.purple),
+              ),),),
+
+
+
+
+
+                      ],
+                    )
+            
+            
+            
+                  ],
+                ),
+              ),
+            ),
+        
+      
+      
+    );
+  }
+}
+
+
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.purple;
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.875,
+        size.width * 0.5, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.9584,
+        size.width * 1.0, size.height * 0.9167);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
